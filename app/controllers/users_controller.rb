494bd517567b73415dbaf04aa18e35a64714fc2e
shinobu7 <shinobu7moon@gmail.com>
2016-11-03 11:15:15 +0000

    challe

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [:edit, :update]
+    before_action :correct_user, only: [:edit, :update]
 
   def show # 追加
    @user = User.find(params[:id])
@@ -23,17 +23,14 @@ class UsersController < ApplicationController
 
 def update
     if @user.update(user_params)
-      # 保存に成功した場合はトップページへリダイレクト
-      redirect_to root_path , notice: 'メッセージを編集しました'
+      flash[:success] = "Updated your Plofile"
+      redirect_to @user, notice: 'メッセージを編集しました'
     else
       # 保存に失敗した場合は編集画面へ戻す
       render 'user'
     end
-  end
+end
 
-
-
-  
   def index
     @user = User.new
     @users = User.all
@@ -46,9 +43,9 @@ private
 def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
-  end
 end
 
 def set_user
     @user = User.find(params[:id])
+end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,8 +6,4 @@ module SessionsHelper
   def logged_in?
     !!current_user
   end
-
-  def store_location
-    session[:forwarding_url] = request.url if request.get?
-  end
 end
