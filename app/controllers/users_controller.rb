94aca185149edc612b7e28c18cc6dfb6fc80b97b
shinobu7 <shinobu7moon@gmail.com>
2016-11-03 23:11:38 +0000

    edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :correct_user, only: [:edit, :update]
+    before_action :current_user, only: [:edit, :update]
 
   def show # 追加
    @user = User.find(params[:id])
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
 
 def update
     if @user.update(user_params)
-      flash[:success] = "Updated your Plofile"
+      flash[:success] = "更新しました"
       redirect_to @user, notice: 'メッセージを編集しました'
     else
       # 保存に失敗した場合は編集画面へ戻す
